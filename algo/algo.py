a0fd686abdc9a1ee751770e5b551e63a7b3d98b1
Michael-Lubinsky <155394365+Michael-Lubinsky@users.noreply.github.com>
2025-06-17 21:31:13 -0700

    Update algo.py

--- algo/algo.py
+++ algo/algo.py
@@ -92,6 +92,27 @@ if __name__ == "__main__":
         print(permutation)
 
 
+
+def has_triplet_with_sum(arr, target):
+    arr.sort()
+    n = len(arr)
+
+    for i in range(n - 2):
+        left, right = i + 1, n - 1
+
+        while left < right:
+            current_sum = arr[i] + arr[left] + arr[right]
+
+            if current_sum == target:
+                return True
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+    return False
+
+
 ###  Detect if in given array there 4 numbers which will sum up to 0 
 #-------------------------------------------------------------------
 def four_sum_to_zero(nums):
